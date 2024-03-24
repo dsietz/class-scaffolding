e383bed7d1091d1598d02077ce36a24aa8120cad
dsietz <davidsietz@yahoo.com>
2024-03-23 20:57:15 -0400

    added core attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
 // #[macro_use]
+// use std::time::{SystemTime, UNIX_EPOCH};
+use chrono::{DateTime, Duration, Months, Utc};
+use uuid::Uuid;
+
+pub enum Intervals {
+    Year,
+    Month,
+    Day,
+}
 
 pub trait Scaffolding {
-    fn hello(&self);
+    fn hello(&self) {}
+
+    /// generates a uuid v4 value
+    fn id() -> String {
+        Uuid::new_v4().to_string()
+    }
+
+    /// adds x days to the timestamp
+    fn add_days(dtm: i64, days: i64) -> i64 {
+        let dt = DateTime::from_timestamp(dtm, 0).unwrap() + Duration::days(days);
+        dt.timestamp()
+    }
+
+    /// adds x years to the timestamp
+    fn add_months(dtm: i64, months: u32) -> i64 {
+        let dt = DateTime::from_timestamp(dtm, 0).unwrap() + Months::new(months);
+        dt.timestamp()
+    }
+
+    /// adds x years to the timestamp
+    fn add_years(dtm: i64, years: u32) -> i64 {
+        let dt = DateTime::from_timestamp(dtm, 0).unwrap() + Months::new(years * 12);
+        dt.timestamp()
+    }
+
+    /// provided the default unix epoch time (UTC) as seconds
+    /// for the timestamp: 9999-12-31 23:59:59
+    fn never() -> i64 {
+        253402261199
+    }
+
+    /// generate the current unix epoch time (UTC) as seconds
+    fn now() -> i64 {
+        Utc::now().timestamp()
+    }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -4,32 +4,69 @@ extern crate scaffolding_macros;
 
 #[cfg(test)]
 mod tests {
+    use chrono::{DateTime, Utc};
     use scaffolding_core::*;
     use scaffolding_macros::*;
 
     #[as_entity]
     #[derive(Debug, Clone, Scaffolding)]
     struct MyEntity {
-        b: bool
+        b: bool,
+        n: i64,
+    }
+
+    impl MyEntity {
+        fn new(arg: bool) -> Self {
+            Self {
+                id: <Self as Scaffolding>::id(),
+                created_dtm: <Self as Scaffolding>::now(),
+                modified_dtm: <Self as Scaffolding>::now(),
+                inactive_dtm: <Self as Scaffolding>::add_days(<Self as Scaffolding>::now(), 90),
+                expired_dtm: <Self as Scaffolding>::add_years(<Self as Scaffolding>::now(), 3),
+                b: arg,
+                n: <Self as Scaffolding>::never(),
+            }
+        }
+
+        fn my_func(&self) -> String {
+            "my function".to_string()
+        }
+    }
+
+    // #[test]
+    // fn test_entity() {
+    //     let entity = MyEntity {
+    //         id: "lorem ipsum".to_string(),
+    //         b: true,
+    //     };
+    //     assert_eq!(entity.id, "lorem ipsum");
+    // }
+
+    #[test]
+    fn test_entity_hello() {
+        let mut entity = MyEntity::new(true);
+        entity.hello();
+        assert_eq!(entity.my_func(), "my function");
     }
 
     #[test]
-    fn test_entity() {
-        let entity = MyEntity {id: "lorem ipsum".to_string(), b: true};
-        entity.hello();
+    fn test_entity_new() {
+        let now = Utc::now().timestamp();
+        let never = 253402261199;
+        let mut entity = MyEntity::new(true);
+
+        // scaffolding attributes
+        assert_eq!(
+            entity.id.len(),
+            "54324f57-9e6b-4142-b68d-1d4c86572d0a".len()
+        );
+        assert_eq!(entity.created_dtm, now);
+        assert_eq!(entity.modified_dtm, now);
+        assert_eq!((entity.inactive_dtm - entity.modified_dtm) / 86400, 90);
+        assert_eq!((entity.expired_dtm - entity.modified_dtm) / 86400, 1095);
+
+        // extended attributes
+        assert_eq!(entity.b, true);
+        assert_eq!(entity.n, never);
     }
-
-    // fn test_macro3() {
-    //     assert_eq!(42, answer());
-    // }
-
-    // // #[test]
-    // // fn test_macro2() {
-    // //     #[derive(HelperAttr)]
-    // //     struct MyObj {
-    // //         #[helper]
-    // //         field: (),
-    // //     }
-    // //     let s = MyObj();
-    // // }
 }
